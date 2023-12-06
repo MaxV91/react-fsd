a3382be4796a5dcdc5cddd2ea312dcbd3834fccf
m.kotsurenko <max.v91@yandex.ru>
2023-12-06 10:51:14 +0300

    theme switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import './index.scss';
+import './styles/index.scss';
 import {Link, Route, Routes} from "react-router-dom";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import { Suspense } from 'react';
-
+import {Suspense} from 'react';
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>toggle</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О Нас</Link>
             <Suspense fallback={<div>Loading...</div>}>
