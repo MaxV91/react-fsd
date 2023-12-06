a904132ce4fd21128af2c4a3c8a82b895d1b5cf2
m.kotsurenko <max.v91@yandex.ru>
2023-12-06 09:17:59 +0300

    lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import {Counter} from "./components/Counter";
 import './index.scss';
+import {Link, Route, Routes} from "react-router-dom";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import { Suspense } from 'react';
 
 
 const App = () => {
     return (
         <div className="app">
-            adasdaDAdds
-            <Counter />
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О Нас</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/'} element={<MainPageAsync />} />
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                </Routes>
+            </Suspense>
         </div>
     )
 }
